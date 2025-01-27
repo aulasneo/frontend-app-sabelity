d2110541d630624f4f9bcdd075bbdd26314be364
jciasenza <jciasenza@aulasneo.com>
2025-01-27 12:32:32 -0300

    SAB-8 Limitar la posibilidad de bajar de plan

--- src/components/data/service.js
+++ src/components/data/service.js
@@ -1,10 +1,26 @@
 import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
-import { getConfig, camelCaseObject } from '@edx/frontend-platform';
+import {
+  getConfig,
+  camelCaseObject,
+  snakeCaseObject,
+} from '@edx/frontend-platform';
 
 export const getUserData = async (username) => {
   try {
     const res = await getAuthenticatedHttpClient().get(
       `${getConfig().LMS_BASE_URL}/api/user/v1/accounts/${username}`
+    );
+    return camelCaseObject(res.data || res.body);
+  } catch (error) {
+    console.error("Error fetching user data:", error);
+    throw error;
+  }
+};
+
+export const getCourses = async () => {
+  try {
+    const res = await getAuthenticatedHttpClient().get(
+    `${getConfig().LMS_BASE_URL}/api/courses/v1/courses/`
     );
     return camelCaseObject(res.data || res.body);
   } catch (error) {
@@ -18,7 +34,7 @@ export const updateUserPlan = async (username, newLimit) => {
   const data = {
     extended_profile: [
       {
-        field_name: 'planLimit',
+        field_name: "planLimit",
         field_value: newLimit,
       },
     ],
@@ -26,11 +42,11 @@ export const updateUserPlan = async (username, newLimit) => {
 
   try {
     const response = await getAuthenticatedHttpClient().patch(url, data, {
-      headers: { 'Content-Type': 'application/merge-patch+json' },
+      headers: { "Content-Type": "application/merge-patch+json" },
     });
     return camelCaseObject(response.data);
   } catch (error) {
-    console.error('Error updating plan:', error);
+    console.error("Error updating plan:", error);
     throw error;
   }
 };
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,16 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { Button, ModalDialog } from "@openedx/paragon";
-import { useIntl } from "@edx/frontend-platform/i18n";
-import { getAuthenticatedUser } from "@edx/frontend-platform/auth";
-import { getConfig } from "@edx/frontend-platform";
-import { getUserData, updateUserPlan } from "../../data/service";
-import PlanCard from "../cards/PlanCard";
-import "./stylesHome.css";
-import messages from "./messages";
+import React, { useState, useEffect } from 'react';
+import { Button, ModalDialog } from '@openedx/paragon';
+import { useIntl } from '@edx/frontend-platform/i18n';
+import { getAuthenticatedUser } from '@edx/frontend-platform/auth';
+import { getConfig } from '@edx/frontend-platform';
+import { getUserData, updateUserPlan, getCourses } from '../data/service';
+import PlanCard from '../cards/PlanCard';
+import './stylesHome.css';
+import messages from './messages';
 
 const Home = () => {
   const intl = useIntl();
   const [planLimit, setPlanLimit] = useState();
+  const [coursesCount, setCoursesCount] = useState();
   const [showModal, setShowModal] = useState(false);
   const user = getAuthenticatedUser();
 
@@ -20,36 +21,49 @@ const Home = () => {
         const data = await getUserData(user.username);
         setPlanLimit(data.extendedProfile[0].fieldValue);
       } catch (error) {
-        console.error("Error fetching user data:", error);
+        console.error('Error fetching user data:', error);
       }
     };
     fetchPlanLimit();
   }, [user.username]);
 
+  useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        const data = await getCourses();
+        setCoursesCount(data.pagination.count);
+      } catch (error) {
+        console.error('Error fetching courses:', error);
+      }
+    };
+    fetchCourses();
+  }, []);
+
   const handlePlanSelect = async (newLimit) => {
-    if (parseInt(newLimit, 10) < parseInt(planLimit, 10)) {
+    const newLimitNumber = parseInt(newLimit, 10);
+    const currentCoursesCount = parseInt(coursesCount, 10);
+
+    if (newLimitNumber < currentCoursesCount) {
       setShowModal(true);
       return;
     }
 
     try {
-      setPlanLimit(newLimit);
       await updateUserPlan(user.username, newLimit);
+      setPlanLimit(newLimit);
     } catch (error) {
-      console.error("Error updating plan:", error);
+      console.error('Error updating plan:', error);
     }
   };
 
   const handleCloseModal = () => setShowModal(false);
 
   const handleBackStudio = () => {
-    const redirectBackStudio = `${
-      getConfig().COURSE_AUTHORING_MICROFRONTEND_URL
-    }/home`;
+    const redirectBackStudio = `${getConfig().COURSE_AUTHORING_MICROFRONTEND_URL}/home`;
     if (redirectBackStudio) {
       window.location.href = redirectBackStudio;
     } else {
-      console.error("Redirect URL is undefined");
+      console.error('Redirect URL is undefined');
     }
   };
 
@@ -59,24 +73,24 @@ const Home = () => {
       features: intl.formatMessage(messages.homeBasicFeatures),
       description: intl.formatMessage(messages.homeBasicDescription),
       price: intl.formatMessage(messages.homeBasicPrice),
-      className: "card-basic",
-      limit: "1",
+      className: 'card-basic',
+      limit: '1',
     },
     {
       title: intl.formatMessage(messages.homeStandardTitle),
       features: intl.formatMessage(messages.homeStandardFeatures),
       description: intl.formatMessage(messages.homeStandardDescription),
       price: intl.formatMessage(messages.homeStandardPrice),
-      className: "card-standard",
-      limit: "3",
+      className: 'card-standard',
+      limit: '3',
     },
     {
       title: intl.formatMessage(messages.homePremiumTitle),
       features: intl.formatMessage(messages.homePremiumFeatures),
       description: intl.formatMessage(messages.homePremiumDescription),
       price: intl.formatMessage(messages.homePremiumPrice),
-      className: "card-premium",
-      limit: "10",
+      className: 'card-premium',
+      limit: '10',
     },
   ];
 
@@ -87,9 +101,7 @@ const Home = () => {
       <h1 className="m-3">{intl.formatMessage(messages.homeTitle)}</h1>
       <h3 className="m-3">
         {currentPlan
-          ? `${intl.formatMessage(
-              messages.suscripcionActualTitle
-            )} ${currentPlan}`
+          ? `${intl.formatMessage(messages.suscripcionActualTitle)} ${currentPlan}`
           : intl.formatMessage(messages.suscripcionAnyMessage)}
       </h3>
       <main>
@@ -125,11 +137,13 @@ const Home = () => {
       >
         <ModalDialog.Body>
           <p className="alertMinPlan">
-            {intl.formatMessage(messages.modalContent, { currentPlan })}
+            {intl.formatMessage(messages.modalExceedsCourses, {
+              currentCourses: coursesCount,
+              newLimit: planLimit,
+            })}
           </p>
-
           <p className="alertMinPlan">
-            {intl.formatMessage(messages.modalContent2)}
+            {intl.formatMessage(messages.modalContent)}
           </p>
         </ModalDialog.Body>
         <ModalDialog.Footer>
